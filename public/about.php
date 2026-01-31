4795f71146a5eb7ac6af19c43108e83be0a089d6
ViCor44 <victor.a.correia@gmail.com>
2026-01-31 21:59:56 +0000

    Teste 3

--- public/about.php
+++ public/about.php
@@ -267,6 +267,7 @@ $maintainers = [
       <ul class="features">
         <li>Gestão de stock de fardas</li>
         <li>Atribuição e registo de itens</li>
+        <li>Atribuição e registo de cacifos</li>
         <li>Relatórios em PDF, Excel e CSV</li>
         <li>Geração de códigos EAN</li>
         <li>Registo de devoluções e avarias</li>
