fd05988ebb15c4a5338d5200a7a571715d027107
ViCor44 <victor.a.correia@gmail.com>
2026-01-31 23:46:08 +0000

    Lint Atualização

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,10 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf64c0f42a180f64dd5c4971478d99adf
 {
-    public static $files = array (
-        '9b38cf48e83f5d8f60375221cd213eee' => __DIR__ . '/..' . '/phpstan/phpstan/bootstrap.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
         'Z' =>
         array (
