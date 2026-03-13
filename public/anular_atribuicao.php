4d43517414f758e39ac7a6fcaf5e2177f3633857
ViCor44 <victor.a.correia@gmail.com>
2026-03-13 20:14:25 +0000

    Botao anular again

--- public/anular_atribuicao.php
+++ public/anular_atribuicao.php
@@ -112,7 +112,7 @@ Cancelar
 </a>
 
 <button type="submit"
-class="px-6 py-2 rounded-lg bg-gray-600 hover:bg-gray-700 text-white font-semibold shadow">
+class="px-6 py-2 rounded-lg bg-red-200 hover:bg-red-300 text-white font-semibold shadow">
 ❌ Confirmar Anulação
 </button>
 
