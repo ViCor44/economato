cad5aa88f760f0a79c96446dcc0523d716865708
ViCor44 <victor.a.correia@gmail.com>
2026-03-19 21:34:22 +0000

    Cont bug

--- reports/generate.php
+++ reports/generate.php
@@ -679,7 +679,7 @@ break;
                 JOIN fardas f ON f.id = fa.farda_id
                 JOIN colaboradores col ON col.id = fa.colaborador_id
                 WHERE fa.data_atribuicao BETWEEN ? AND ?
-                  AND fa.estado <> 'devolvida_confirmada'
+                  AND fa.estado IN ('atribuida', 'marcada_devolucao', 'em_divida')
                 GROUP BY col.id
                 ORDER BY total DESC
                 LIMIT ?
@@ -701,7 +701,7 @@ break;
                 JOIN colaboradores col ON col.id = fa.colaborador_id
                 JOIN departamentos d ON d.id = col.departamento_id
                 WHERE fa.data_atribuicao BETWEEN ? AND ?
-                  AND fa.estado <> 'devolvida_confirmada'
+                  AND fa.estado IN ('atribuida', 'marcada_devolucao', 'em_divida')
                 GROUP BY d.id
                 ORDER BY total DESC
                 LIMIT ?
