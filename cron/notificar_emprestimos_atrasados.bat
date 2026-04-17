d9979fcf66e90ac60e740aa68027c755160b610d
ViCor44 <victor.a.correia@gmail.com>
2026-04-17 23:33:08 +0100

    Corrige execucao do cron de notificacoes

--- cron/notificar_emprestimos_atrasados.bat
+++ cron/notificar_emprestimos_atrasados.bat
@@ -1,4 +1,4 @@
 @echo off
 :: Notificar colaboradores com emprestimos de farda com mais de 15 dias.
 :: Pode correr diariamente (ex: 08:00); o script limita para 1 aviso por emprestimo a cada 7 dias.
-"C:\xampp\php\php.exe" "C:\xampp\economato\cron\notificar_emprestimos_atrasados.php"
+"C:\xampp\php\php.exe" "C:\xampp\htdocs\economato\cron\notificar_emprestimos_atrasados.php"
