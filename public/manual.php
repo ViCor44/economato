5be6f4001b64bf4c8a5619f7078589fd86dd3d51
ViCor44 <victor.a.correia@gmail.com>
2026-05-09 22:15:17 +0100

    feat: substituir icone capa do manual pelo logo CrewGest

--- public/manual.php
+++ public/manual.php
@@ -33,9 +33,19 @@ $html = <<<HTML
     padding-top: 60mm;
   }
   .cover-logo {
-    font-size: 48pt;
-    color: #2563eb;
-    margin-bottom: 8mm;
+    margin-bottom: 10mm;
+  }
+  .cover-logo-box {
+    display: inline-block;
+    width: 22mm;
+    height: 22mm;
+    border-radius: 5mm;
+    background: #1d4ed8;
+    color: #ffffff;
+    font-size: 22pt;
+    font-weight: 700;
+    line-height: 22mm;
+    text-align: center;
   }
   .cover-title {
     font-size: 28pt;
@@ -224,7 +234,7 @@ $html = <<<HTML
 
 <!-- CAPA -->
 <div class="cover">
-  <div class="cover-logo">&#9874;</div>
+  <div class="cover-logo"><div class="cover-logo-box">Cr</div></div>
   <div class="cover-title">CrewGest</div>
   <div class="cover-subtitle">Sistema de Gestão de Fardamento e Colaboradores</div>
   <hr class="cover-line">
