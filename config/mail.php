47c36ed65ed9f080b87cf616c74c88449e23e359
ViCor44 <victor.a.correia@gmail.com>
2026-07-16 16:41:46 +0100

    Env

--- config/mail.php
+++ config/mail.php
@@ -4,7 +4,7 @@ return [
     'host' => 'smtp.gmail.com',
     'port' => 587,
     'username' => 'slide.rocketchat@gmail.com',
-    'password' => 'abel jacr oqpd hdit',
+    'password' => getenv('GMAIL_APP_PASSWORD') ?: '',
     'from_email' => 'slide.rocketchat@gmail.com',
     'from_name' => 'CrewGest',
     'secure' => 'ssl', // or 'ssl'
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '054ff88649167a716ae712a4da67a54a1115252d',
+        'reference' => 'dfdc1497415ee136d834e1b8e63b46320b1f4428',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '054ff88649167a716ae712a4da67a54a1115252d',
+            'reference' => 'dfdc1497415ee136d834e1b8e63b46320b1f4428',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
